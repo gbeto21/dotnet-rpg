55f3b6361a5abcc38e8fcee70f2f1f23a14facb4
Alberto González Balderas <albertoglezb@hotmail.com>
2020-07-04 18:56:03 -0500

    The model Weapon was implemented.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,6 +8,8 @@ namespace dotnet_rpg.Data
         public DbSet<Character> Characters { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Weapon> Weapons { get; set; }
         public DataContext(DbContextOptions<DataContext> pOptions): base(pOptions){}
     }
 }
